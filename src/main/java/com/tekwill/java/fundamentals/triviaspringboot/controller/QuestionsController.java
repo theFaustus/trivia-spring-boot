c93b0f92f21275bb73fca4eab7b25502d2a27f63
ipascari <jhoonnyc@gmail.com>
2021-03-05 20:46:31 +0200

    fix naming of controller methods

--- src/main/java/com/tekwill/java/fundamentals/triviaspringboot/controller/QuestionsController.java
+++ src/main/java/com/tekwill/java/fundamentals/triviaspringboot/controller/QuestionsController.java
@@ -23,32 +23,32 @@ import java.util.List;
 public class QuestionsController {
     private final QuestionService questionService;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
     public ResponseEntity<List<Question>> getAllQuestions(){
         final List<Question> all = questionService.getAll();
         return ResponseEntity.ok(all);
     }
 
-    @GetMapping(value = "/by-level/{level}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/by-level/{level}")
     public ResponseEntity<List<Question>> getQuestionsByLevel(@PathVariable Integer level){
         final List<Question> byLevel = questionService.getQuestionsByLevel(level);
         return ResponseEntity.ok(byLevel);
     }
 
-    @GetMapping(value = "/{questionId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{questionId}")
     public ResponseEntity<Question> getQuestionById(@PathVariable Long questionId){
         final Question questionById = questionService.getQuestionById(questionId);
         return ResponseEntity.ok(questionById);
     }
 
-    @DeleteMapping(value = "/{questionId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "/{questionId}")
     public ResponseEntity<?> deleteQuestionById(@PathVariable Long questionId){
         questionService.deleteById(questionId);
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping( produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> deleteQuestionById(Question question){
+    @PostMapping
+    public ResponseEntity<String> createQuestion(Question question){
         question.getAnswers().forEach(q -> q.setQuestion(question));
         final Long newQuestionId = questionService.save(question);
         return ResponseEntity.created(URI.create("/questions/" + newQuestionId)).body("Question created");
